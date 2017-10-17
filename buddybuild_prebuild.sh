79e116c4fbfbf4b1fb2d358639407183bb6bec26
Roland Hordos <rolandhordos@me.com>
2017-10-17 00:14:02 -0700

    Back to what worked, yarn global add jest.  Skipping build artifacts which did not work.

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -1,6 +1,2 @@
-echo "=== pwd:" `pwd` " ==="
-# yarn global add jest
-./node_modules/.bin/jest
-# Jest won't create our folder structure for us, so lets create it first
-#mkdir buddybuild_artifacts && mkdir buddybuild_artifacts/Jest
-#jest --outputFile=buddybuild_artifacts/Jest/jest.json --json
+yarn global add jest
+jest

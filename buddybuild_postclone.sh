557bc9e92129ff76e835242d01f82e0125f3900b
Roland Hordos <rolandhordos@me.com>
2017-10-16 23:19:34 -0700

    Attempt npm install

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -3,8 +3,8 @@ echo "=== PATH ==="
 echo $PATH
 echo "=== LS ==="
 ls
-echo "=== node --version ==="
-node --version
+echo "=== npm install jest ==="
+npm install jest
 
 # Jest won't create our folder structure for us, so lets create it first
 mkdir buddybuild_artifacts && mkdir buddybuild_artifacts/Jest

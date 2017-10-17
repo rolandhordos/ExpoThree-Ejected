e37e1f3d8d3047071775bc4ad465194a00736f1c
Roland Hordos <rolandhordos@me.com>
2017-10-17 00:27:33 -0700

    That ran jest, can we make this more concise?

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -1,9 +1,4 @@
-echo "=== pwd:" `pwd` " ==="
-
 yarn global add jest
-# Jest won't create our folder structure for us, so lets create it first
 mkdir buddybuild_artifacts && mkdir buddybuild_artifacts/Jest
-
+#jest --runInBand
 jest --outputFile=buddybuild_artifacts/Jest/jest.json --json
-# Some users might want to just run
-# npm test instead, depending on how they have everything set up

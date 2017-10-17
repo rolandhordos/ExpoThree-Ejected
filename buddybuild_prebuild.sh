12337ff15bcae4d3b9498a8bda9ce63dd0da0db4
Roland Hordos <rolandhordos@me.com>
2017-10-16 23:48:53 -0700

    Moved the post clone into a pre build script

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -1,7 +1,11 @@
+#!/usr/bin/env bash
+
 echo "=== pwd:" `pwd` " ==="
+
 # Jest won't create our folder structure for us, so lets create it first
 mkdir buddybuild_artifacts && mkdir buddybuild_artifacts/Jest
-# jest --outputFile=buddybuild_artifacts/Jest/jest.json --json
+
+jest --outputFile=buddybuild_artifacts/Jest/jest.json --json
 # Some users might want to just run
 # npm test instead, depending on how they have everything set up
 #echo "=== npm test ==="

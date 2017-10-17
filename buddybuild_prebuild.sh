8e25dac33d3631c7c6f680ace5dfd43fd3924d49
Roland Hordos <rolandhordos@me.com>
2017-10-17 00:08:01 -0700

    Attempt to use local jest

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -1,9 +1,6 @@
 echo "=== pwd:" `pwd` " ==="
-yarn global add jest
+# yarn global add jest
+./node_modules/.bin/jest
 # Jest won't create our folder structure for us, so lets create it first
-mkdir buddybuild_artifacts && mkdir buddybuild_artifacts/Jest
-jest --outputFile=buddybuild_artifacts/Jest/jest.json --json
-# Some users might want to just run
-# npm test instead, depending on how they have everything set up
-#echo "=== npm test ==="
-#npm test
+#mkdir buddybuild_artifacts && mkdir buddybuild_artifacts/Jest
+#jest --outputFile=buddybuild_artifacts/Jest/jest.json --json

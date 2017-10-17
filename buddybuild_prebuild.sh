03c6c52d6a3c560317344158a40298d6307764c1
Roland Hordos <rolandhordos@me.com>
2017-10-16 23:59:51 -0700

    yarn global add jest

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -1,8 +1,7 @@
 echo "=== pwd:" `pwd` " ==="
-
+yarn global add jest
 # Jest won't create our folder structure for us, so lets create it first
 mkdir buddybuild_artifacts && mkdir buddybuild_artifacts/Jest
-
 jest --outputFile=buddybuild_artifacts/Jest/jest.json --json
 # Some users might want to just run
 # npm test instead, depending on how they have everything set up

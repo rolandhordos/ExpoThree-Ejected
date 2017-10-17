e0f4f0dcbee311e70121327955c260d2a60cdea7
Roland Hordos <rolandhordos@me.com>
2017-10-16 23:32:00 -0700

    Attempt Xcode build phase scripting instead

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -6,5 +6,5 @@ mkdir buddybuild_artifacts && mkdir buddybuild_artifacts/Jest
 # jest --outputFile=buddybuild_artifacts/Jest/jest.json --json
 # Some users might want to just run
 # npm test instead, depending on how they have everything set up
-echo "=== npm test ==="
-npm test
+#echo "=== npm test ==="
+#npm test
